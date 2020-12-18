f10e06d9c06618aba1b69cb64fa3b73b944a6565
Stephen Webber <offgridauthor@gmail.com>
2020-12-17 23:26:03 -0800

    additional bits here and there

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,6 @@
 /* global Cart */
 'use strict';
 
-// Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 var table = document.getElementById('cart');
 table.addEventListener('click', removeItemFromCart);
 var cart;
@@ -11,36 +10,47 @@ function loadCart() {
   cart = new Cart(cartItems);
 }
 
-// Make magic happen --- re-pull the Cart, clear out the screen and re-draw it
 function renderCart() {
   loadCart();
   clearCart();
   showCart();
 }
 
-// TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
-  
+function clearCart() {
+  table.innerHTML = '';
+}
+
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
+  var itemEl = document.getElementById('cart');
+  for (var i=0;i<cart.items.length; i++) {
+    var row = document.createElement('tr');
+    var data = document.createElement('td');
+    itemEl.appendChild(row);
+    data.textContent = 'X';
+    data.setAttribute('X', `   ${i}`);
+    row.appendChild(data);
 
-  // TODO: Find the table body
+    data = document.createElement('td');
+    data.textContent = `${cart.items[i].quantity}`;
+    row.appendChild(data);
+    data = document.createElement('td');
+    data.textContent = `${cart.items[i].product}`;
+    row.appendChild(data);
+  }
 
-  // TODO: Iterate over the items in the cart
-  // TODO: Create a TR
-  // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+
 
 }
 
 function removeItemFromCart(event) {
-Cart.removeItem(this.item);  //? probably.----
+  Cart.removeItem(this.item);  //? probably.----
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
 
   // TODO. Save the cart back to local storage
-Cart.saveToLocalStorage();
+  Cart.saveToLocalStorage();
   // TODO. Re-draw the cart table
-showCart(); // ?--- maybe
+  showCart(); // ?--- maybe
 }
 
 // This will initialize the page and draw the cart on screen
